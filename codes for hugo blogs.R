07963c7219e17b954195ecfd1be73a74de8c7c97
Sayed Masood Sadat <smsadat@users-iMac.local>
2018-06-13 16:59:21 +0430

    upload all hugo themes of interest

---
+++ codes for hugo blogs.R
@@ -0,0 +1,78 @@
+setwd("~/Documents/GitHub/hugoblogs/hyde-hyde")
+blogdown::new_site(theme = "htr3n/hyde-hyde")
+
+setwd("~/Documents/GitHub/hugoblogs/zen")
+blogdown::new_site(theme = "frjo/hugo-theme-zen")
+
+setwd("~/Documents/GitHub/hugoblogs/academic")
+blogdown::new_site(theme = "gcushen/hugo-academic")
+
+setwd("~/Documents/GitHub/hugoblogs/boostrap_prem")
+blogdown::new_site(theme = "appernetic/hugo-bootstrap-premium")
+
+setwd("~/Documents/GitHub/hugoblogs/coder")
+blogdown::new_site(theme = "luizdepra/hugo-coder")
+
+setwd("~/Documents/GitHub/hugoblogs/hydey")
+blogdown::new_site(theme = "enten/hyde-y")
+
+setwd("~/Documents/GitHub/hugoblogs/bootstrap")
+blogdown::new_site(theme = "Xzya/hugo-bootstrap")
+
+setwd("~/Documents/GitHub/hugoblogs/foundation")
+blogdown::new_site(theme = "hdcdstr8fwd/foundation-theme")
+
+setwd("~/Documents/GitHub/hugoblogs/nuo")
+blogdown::new_site(theme = "laozhu/hugo-nuo")
+
+setwd("~/Documents/GitHub/hugoblogs/bootstrap4")
+blogdown::new_site(theme = "alanorth/hugo-theme-bootstrap4-blog")
+
+setwd("~/Documents/GitHub/hugoblogs/simplicity")
+blogdown::new_site(theme = "eshlox/simplicity")
+
+setwd("~/Documents/GitHub/hugoblogs/octopress")
+blogdown::new_site(theme = "parsiya/Hugo-Octopress")
+
+setwd("~/Documents/GitHub/hugoblogs/smorg")
+blogdown::new_site(theme = "solutionroute/hugo-smorg")
+
+setwd("~/Documents/GitHub/hugoblogs/mainroad")
+blogdown::new_site(theme = "Vimux/Mainroad")
+
+setwd("~/Documents/GitHub/hugoblogs/blackburn")
+blogdown::new_site(theme = "yoshiharuyamashita/blackburn")
+
+setwd("~/Documents/GitHub/hugoblogs/kraiklyn")
+blogdown::new_site(theme = "jsnjack/kraiklyn")
+
+setwd("~/Documents/GitHub/hugoblogs/phlat")
+blogdown::new_site(theme = "nraboy/hugo-phlat-theme")
+
+setwd("~/Documents/GitHub/hugoblogs/wave")
+blogdown::new_site(theme = "syui/hugo-theme-wave")
+
+setwd("~/Documents/GitHub/hugoblogs/hyde")
+blogdown::new_site(theme = "spf13/hyde")
+
+# setwd("~/Documents/GitHub/hugoblogs/learn")
+# blogdown::new_site(theme = "matcornic/hugo-theme-learn")
+
+# setwd("~/Documents/GitHub/hugoblogs/strange")
+# blogdown::new_site(theme = "ExchangeRate-API/strange-case")
+
+setwd("~/Documents/GitHub/hugoblogs/foundation6")
+blogdown::new_site(theme = "htkoca/theme-hugo-foundation6")
+
+setwd("~/Documents/GitHub/hugoblogs/amp")
+blogdown::new_site(theme = "wildhaber/gohugo-amp")
+
+setwd("~/Documents/GitHub/hugoblogs/paper")
+blogdown::new_site(theme = "eueung/hugo-paper-now")
+
+setwd("~/Documents/GitHub/hugoblogs/twentyfourteen")
+blogdown::new_site(theme = "jaden/twentyfourteen")
+
+# setwd("~/Documents/GitHub/hugoblogs/greyshade")
+# blogdown::new_site(theme = "cxfksword/greyshade")
+
